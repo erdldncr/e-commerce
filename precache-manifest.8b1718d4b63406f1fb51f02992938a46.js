1046ce93eb70ab1a27d0f6fb568505e2aad73075
erdldncr <erdldncr@gmail.com>
2021-05-24 20:15:08 +0100

    Updates

---
+++ precache-manifest.8b1718d4b63406f1fb51f02992938a46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82266bf877c4f048f0f2",
+    "url": "/e-commerce-with-react/static/css/main.19332fab.chunk.css"
+  },
+  {
+    "revision": "82266bf877c4f048f0f2",
+    "url": "/e-commerce-with-react/static/js/main.82266bf8.chunk.js"
+  },
+  {
+    "revision": "f200d2d2404510fb7bc5",
+    "url": "/e-commerce-with-react/static/css/1.73053241.chunk.css"
+  },
+  {
+    "revision": "f200d2d2404510fb7bc5",
+    "url": "/e-commerce-with-react/static/js/1.f200d2d2.chunk.js"
+  },
+  {
+    "revision": "1ebbc2124be433b72267",
+    "url": "/e-commerce-with-react/static/js/runtime~main.1ebbc212.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/e-commerce-with-react/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "11d724cde2b4aadc80e3a9a3471447d4",
+    "url": "/e-commerce-with-react/index.html"
+  }
+];
